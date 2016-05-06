dee2315c44999624e7b9e50c9de6b83547dae843
Sinchaisri <psinchaisri@deloitte.com>
2016-05-06 16:19:09 -0400

    Initial upload

---
+++ stratifiedsplit.r
@@ -0,0 +1,11 @@
+# StratifiedSplit
+
+setwd("C:/Users/psinchaisri/Projects/StratifiedSplit")
+
+# Required packages
+install.packages("devtools")
+library(devtools)
+devtools::install_github("klutometis/roxygen")
+library(roxygen2)
+
+# Create my package directory
